ba7d54af09449a216fbe504a6927506d1d312f14
Johnny <johnnyfabiancardozo24@gmail.com>
2026-07-30 10:30:08 -0500

    Add files via upload

---
+++ Backend/Sprint 1/Models/Entidades/caracteristicas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntidadesJson.clases
+{
+    public class caracteristicas
+    {
+        public int id_caracteristica { get; set; }
+        public string nombre { get; set; }
+    }
+}
