be0f73b5d34dce931adda93a9cec88d787a1cf4e
Miguel Gómez 117 <miguelingogo123@gmail.com>
2026-07-30 10:59:31 -0500

    ola!

---
+++ Backend/Sprint 1/Models/barrio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntidadesJson.clases
+{
+    public class barrio
+    {
+        public int id_barrio { get; set; }
+        public string nombre { get; set; }
+        public int id_ciudad { get; set; }
+        [Required(ErrorMessage = "Hola")]
+        public int id_localidad { get; set; }
+    }
+}
---
+++ Backend/Sprint 1/Models/rol.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntidadesJson.clases
+{
+    public class rol
+    {
+        public int id_rol { get; set; }
+        public string nombre { get; set; }
+    }
+}
