1f4933b2c71658c0401d4c876c3cfcb50e0feff1
Aaron Quintana <aaron.quintana7777@gmail.com>
2026-07-31 10:53:35 -0500

    Se agregan los respectivos data annotations

---
+++ Backend/Sprint 1/Models/Entidades/Entidades con data annotations/caracteristicas.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Caracteristica
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int IdCaracteristica { get; set; }
+
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
+    [StringLength(100, ErrorMessage = "El nombre no puede superar los 100 caracteres.")]
+    public string Nombre { get; set; }
+}
---
+++ Backend/Sprint 1/Models/Entidades/Entidades con data annotations/ciudad.cs
@@ -0,0 +1,17 @@
+﻿using rever.Models.Entidades;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Ciudad
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int IdCiudad { get; set; }
+
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
+    [StringLength(100, ErrorMessage = "El nombre no puede superar los 100 caracteres.")]
+    public string Nombre { get; set; }
+
+
+    public ICollection<Barrio> Barrios { get; set; }
+}
