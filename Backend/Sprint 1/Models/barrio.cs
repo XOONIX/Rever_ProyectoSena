69077401f4b85d9f493fdd2c184862206b2aa821
Johnny <johnnyfabiancardozo24@gmail.com>
2026-08-03 13:32:46 -0500

    Refactor barrio class to Barrio with data annotations

--- Backend/Sprint 1/Models/barrio.cs
+++ Backend/Sprint 1/Models/barrio.cs
@@ -1,18 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace EntidadesJson.clases
+public class Barrio
 {
-    public class barrio
-    {
-        public int id_barrio { get; set; }
-        public string nombre { get; set; }
-        public int id_ciudad { get; set; }
-        [Required(ErrorMessage = "Hola")]
-        public int id_localidad { get; set; }
-    }
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int IdBarrio { get; set; }
+
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
+    [StringLength(100, ErrorMessage = "El nombre no puede superar los 100 caracteres.")]
+    public string Nombre { get; set; }
+
+    [Required]
+    public int IdCiudad { get; set; }
+
+    [Required]
+    public int IdLocalidad { get; set; }
+
+    [ForeignKey(nameof(IdCiudad))]
+    public Ciudad Ciudad { get; set; }
+
+    [ForeignKey(nameof(IdLocalidad))]
+    public Localidad Localidad { get; set; }
 }
