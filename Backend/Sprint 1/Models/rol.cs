dc7842b1d3ab50fc096be8e274fde7647bf1b492
Miguel Gómez 117 <miguelingogo123@gmail.com>
2026-08-03 13:47:29 -0500

    Update rol.cs

--- Backend/Sprint 1/Models/rol.cs
+++ Backend/Sprint 1/Models/rol.cs
@@ -9,7 +9,11 @@ namespace EntidadesJson.clases
 {
     public class rol
     {
+        [Key]
         public int id_rol { get; set; }
+
+        [Required(ErrorMessage = "El nombre del rol es obligatorio.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre del rol debe tener entre 3 y 50 caracteres.")]
         public string nombre { get; set; }
     }
 }
