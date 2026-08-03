258f0a0b55c5932adf9164073684e0eba9bd6d12
Kevin Santiago Arevalo Trujillo <kevinarevalo405@gmail.com>
2026-08-03 12:57:42 -0500

    subida de la clase «inmueble_caracteristicas» con atributos de validación
    Se han añadido a la carpeta la nueva clase con sus respectivas anotaciones de datos para la validación.

---
+++ Backend/Sprint 1/Models/Entidades/inmueble_caracteristica.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EntidadesJson.clases
+{
+    [Table("inmueble_caracteristica")]
+    public class InmuebleCaracteristica
+    {
+        [Key]
+        [Column("id_inmueble", Order = 0)]
+        public int IdInmueble { get; set; }
+
+        [Key]
+        [Column("id_caracteristica", Order = 1)]
+        public int IdCaracteristica { get; set; }
+
+        // Propiedades de navegación para las Llaves Foráneas
+        [ForeignKey("IdInmueble")]
+        public virtual inmueble inmueble { get; set; } = null!;
+
+        [ForeignKey("Idcaracteristica")]
+        public virtual caracteristicas caracteristicas { get; set; } = null!;
+    }
+}
