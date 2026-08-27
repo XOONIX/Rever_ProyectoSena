3fb1acaf63fcae5cb0acfeaab3e678b3d7c3b2b8
Kevin Santiago Arevalo Trujillo <kevinarevalo405@gmail.com>
2026-08-27 00:03:31 -0500

    Add files via upload

---
+++ Backend/Rever/Models/Login.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace rever.Models
+{
+    public class Login
+    {
+        [Required(ErrorMessage = "El usuario es obligatorio")]
+        public string Usuario { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        [MinLength(6, ErrorMessage = "La contraseña debe tener al menos 6 caracteres")]
+        public string Contraseña { get; set; }
+    }
+}
